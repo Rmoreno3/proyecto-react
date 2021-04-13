764af16f15f4dca16de1080d8394d8f14a5e5e94
Reynaldo Moreno <reynaldomoreno173@gmail.com>
2021-04-12 23:08:46 -0400

    Updates

---
+++ precache-manifest.20d1459c17b410d26f1cecbcf02fe197.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "9dadd9775591d85e4b70",
+    "url": "/proyecto-react/static/css/main.179b0b52.chunk.css"
+  },
+  {
+    "revision": "9dadd9775591d85e4b70",
+    "url": "/proyecto-react/static/js/main.9dadd977.chunk.js"
+  },
+  {
+    "revision": "add4f474ee5b94c8d42b",
+    "url": "/proyecto-react/static/js/runtime~main.add4f474.js"
+  },
+  {
+    "revision": "6c097f988830e6717355",
+    "url": "/proyecto-react/static/css/2.50a39356.chunk.css"
+  },
+  {
+    "revision": "6c097f988830e6717355",
+    "url": "/proyecto-react/static/js/2.6c097f98.chunk.js"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/proyecto-react/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/proyecto-react/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/proyecto-react/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/proyecto-react/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "bdac359931bddba2478eeea26138c561",
+    "url": "/proyecto-react/static/media/error.bdac3599.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/proyecto-react/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "b0137a342a0768e8f4fbb6661f972377",
+    "url": "/proyecto-react/index.html"
+  }
+];
